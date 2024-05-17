dcda382302495c8807b1827d2fa5198fa8721c2a
Bryan Moreira <161074974+BryanM-22@users.noreply.github.com>
2024-05-17 10:50:53 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colas_Tarea1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> miFila = new Queue<string>();
+
+            Console.Write("Ingrese el numero de personas en la fila: ");
+            int numeroDePersonas = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numeroDePersonas; i++)
+            {
+                Console.Write("Ingrese el nombre de la persona " + (i + 1) + ":");
+                string nombre = Console.ReadLine();
+                miFila.Enqueue(nombre);
+            }
+
+            for (int cont = 0; cont < numeroDePersonas; cont++)
+            {
+                Console.WriteLine("La cajera atendera a: " + miFila.Dequeue());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
